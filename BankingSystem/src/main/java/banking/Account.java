1503862f6955465e0a2800f195a5ef35c02ccdc0
DiptiNwm <86035330+DiptiNwm@users.noreply.github.com>
2021-12-09 00:45:19 +0530

    Update Account.java

--- BankingSystem/src/main/java/banking/Account.java
+++ BankingSystem/src/main/java/banking/Account.java
@@ -15,37 +15,46 @@ public abstract class Account {
 	private Long accountNumber;
 	private int pin;
 	private double balance;
+	
 
 	protected Account(AccountHolder accountHolder, Long accountNumber, int pin, double startingDeposit) {
-		// complete the constructor
+		this.accountHolder=accountHolder;
+		this.accountNumber=accountNumber;
+		this.pin=pin;
+		this.balance=startingDeposit;
 	}
 
 	public AccountHolder getAccountHolder() {
 		// complete the function
-        return null;
+        return this.accountHolder;
 	}
 
 	public boolean validatePin(int attemptedPin) {
 		// complete the function
-        return true;
+        return attemptedPin<=5;
 	}
 
 	public double getBalance() {
 		// complete the function
-        return -1;
+        return this.balance;
 	}
 
 	public Long getAccountNumber() {
 		// complete the function
-        return -1L;
+        return this.accountNumber;
 	}
 
 	public void creditAccount(double amount) {
-		// complete the function
+		return balance+=amount;
 	}
 
 	public boolean debitAccount(double amount) {
 		// complete the function
-        return true;
+		if(balance>amount)
+		{
+			balance-=amount;
+			return true;
+		}
+        return false;
 	}
 }
